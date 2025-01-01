60ad5841225ad507899925b72a78f1b9d93627c0
shaozk <913667678@qq.com>
2025-01-01 23:48:00 +0800

    [init][引入mdf解析库][#0]

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mdfplot
-mdf文件曲线绘图工具
+mdf文件曲线绘图工具，使用开源第三方库`mdflib`实现。
+
+
+
+## 第三方库
+* [mdflib](https://github.com/ihedvall/mdflib) 
+
+
+
+## 参考
+* [linux安装mdflib库的教程（更新中）](https://blog.csdn.net/LEERR_/article/details/138539967)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
+
+#include "mdf/mdfreader.h"
 
 int main()
 {
     std::cout << "hello mdfplot" << std::endl;
+    std::string mdfpath = "";
+    mdf::MdfReader reader(mdfpath); 
     return 0;
 }
