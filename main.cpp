844370be93e932040fe0607c187534c219b98fe3
shaozk <913667678@qq.com>
2025-01-03 00:10:34 +0800

    [init][引入qt库][#0]

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
+#include "MainWindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
 #include <iostream>
 #include <string>
 
 #include "mdf/mdfreader.h"
 
-int main()
+
+int main(int argc, char *argv[])
 {
     std::cout << "hello mdfplot" << std::endl;
     std::string mdfpath = "";
-    mdf::MdfReader reader(mdfpath); 
-    return 0;
+    mdf::MdfReader reader(mdfpath);
+
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "mdfplot_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+
 }
